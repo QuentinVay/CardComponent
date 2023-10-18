409502ea2536f617ee47107ff697a63883f9ed38
Quentin Vayssieres <quentin.vayssieres@gmail.com>
2023-10-18 11:04:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Card from './Card'
+import '/src/Card.scss'
+
+function App() {
+ 
+
+  return (
+    <>
+  <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,33 @@
+function Card() {
+
+
+    return (
+        <>
+            <div className="test">
+                <div className="card">
+                    <div className="imageCard"></div>
+                    <div className="paraCard">
+                        <div className="titleCard">
+                            <h1>Rozes</h1>
+                        </div>
+                        <div className="textCard">
+                            <p>Under the grave 2016</p>
+                        </div>
+                    </div>
+
+                    <div className="infCard">
+                        <p>Rate this album</p>
+                    </div>
+                    <div className="noticeCard">
+                        <p>★★★★★</p>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </>
+    )
+}
+
+export default Card
